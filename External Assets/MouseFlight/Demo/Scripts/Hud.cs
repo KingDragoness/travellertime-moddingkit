243c03b3809e839919818ef19cd9c8f4d738a728
KingDragoness <king72dragoness@gmail.com>
2020-08-26 13:58:47 +0700

    Modkit standalone support

---
+++ External Assets/MouseFlight/Demo/Scripts/Hud.cs
@@ -0,0 +1,70 @@
+﻿//
+// Copyright (c) Brian Hernandez. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+using UnityEngine;
+
+namespace MFlight.Demo
+{
+    public class Hud : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private MouseFlightController mouseFlight = null;
+
+        [Header("HUD Elements")]
+        [SerializeField] private RectTransform boresight = null;
+        [SerializeField] private RectTransform mousePos = null;
+
+        private Camera playerCam = null;
+
+        private void Awake()
+        {
+            if (mouseFlight == null)
+                Debug.LogError(name + ": Hud - Mouse Flight Controller not assigned!");
+
+            playerCam = FindObjectOfType<Cinemachine.CinemachineBrain>().GetComponent<Camera>();
+
+            if (playerCam == null)
+                Debug.LogError(name + ": Hud - No camera found on assigned Mouse Flight Controller!");
+        }
+
+        private void Update()
+        {
+            if (mouseFlight == null || playerCam == null)
+                return;
+
+            UpdateGraphics(mouseFlight);
+        }
+
+        private void UpdateGraphics(MouseFlightController controller)
+        {
+            if (boresight != null)
+            {
+                Vector2 myPositionOnScreen = playerCam.WorldToScreenPoint(controller.BoresightPos);
+                float scaleFactor = 1;
+
+                Vector2 finalPosition = new Vector2(myPositionOnScreen.x / scaleFactor, myPositionOnScreen.y / scaleFactor);
+
+                boresight.anchoredPosition = finalPosition;
+                //boresight.gameObject.SetActive(boresight.position.z > 1f);
+            }
+
+            if (mousePos != null)
+            {
+                Vector2 myPositionOnScreen = playerCam.WorldToScreenPoint(controller.MouseAimPos);
+                float scaleFactor = 1;
+
+                Vector2 finalPosition = new Vector2(myPositionOnScreen.x / scaleFactor, myPositionOnScreen.y / scaleFactor);
+
+                mousePos.anchoredPosition = finalPosition;
+               
+            }
+        }
+
+        public void SetReferenceMouseFlight(MouseFlightController controller)
+        {
+            mouseFlight = controller;
+        }
+    }
+}
