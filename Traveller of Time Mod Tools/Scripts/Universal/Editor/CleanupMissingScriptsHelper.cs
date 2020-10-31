969ff6297cd0fcf79bb0b33104e69fd485f04e84
KingDragoness <king72dragoness@gmail.com>
2020-10-31 18:11:26 +0700

    0.2.2. miniup

---
+++ Traveller of Time Mod Tools/Scripts/Universal/Editor/CleanupMissingScriptsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CleanupMissingScriptsHelper
+{
+    [MenuItem("Edit/Cleanup Missing Scripts")]
+    static void CleanupMissingScripts()
+    {
+        for (int i = 0; i < Selection.gameObjects.Length; i++)
+        {
+            GameObjectUtility.RemoveMonoBehavioursWithMissingScript(Selection.gameObjects[i]);
+        }
+    }
+
+
+    [MenuItem("Edit/Recursive Cleanup Missing Scripts")]
+    static void RecursiveCleanupMissingScripts()
+    {
+        Transform[] allTransforms = Selection.gameObjects[0].GetComponentsInChildren<Transform>(true);
+
+        for (int i = 0; i < allTransforms.Length; i++)
+        {
+            var gameObject = allTransforms[i].gameObject;
+
+            GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameObject);
+
+        }
+    }
+}
